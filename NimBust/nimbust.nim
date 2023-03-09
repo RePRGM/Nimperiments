630cb2c4dd41ec56fb4f0a4448abceb3f3a54011
RePRGM <92693582+RePRGM@users.noreply.github.com>
2023-03-09 14:01:05 -0500

    Update nimbust.nim

--- NimBust/nimbust.nim
+++ NimBust/nimbust.nim
@@ -1,96 +1,200 @@
+import argparse
 import std/[
   os,
   httpclient,
   strutils,
   terminal,
   strformat,
-  threadpool,
   sequtils,
-  times
+  times,
+  locks
 ]
 
-from std/net import newContext, SslCVerifyMode
+from std/net import newContext, SslCVerifyMode, TimeoutError
+from std/httpcore import HttpHeaderValues, contains
+from std/uri import encodeUrl
+from std/osproc import execCmd
 
-# params[0] = url
-# params[1] = wordlist
-# params[2] = number of threads
-
+type ThreadData = tuple[doAppend: bool, wordlist: seq[string], baseUrl: string, extension: seq[string], output: string, rDelay: float, timeout: int, userAgent: string, quietMode: bool]
+  
 var 
-  params = commandLineParams()
-  nimbust = extractFilename(getAppFilename())
-  fullList: seq[string]
+  L: Lock
+  version = "1.0.0"
+  
+L.initLock()
 
-proc parseWordList(wordlist: string, thrNum: int): seq[seq[string]] =
+proc parseWordList(wordlist: string, thrNum: int): seq[seq[string]]
+proc bruteForce(options: tuple): void {.thread.}
+proc main(options: tuple): void
+
+when isMainModule:
+  var p = newParser:
+    flag("-a", "--append", help="Append / to every request")
+    flag("-r", "--recursion", help="Turn recursion on", hidden=true)
+    flag("-q", "--quiet", help="Turn off printing to stdout")
+    option("-t", "--threads", help="Specify number of threads to use. If value is under 5, option is ignored and single thread mode is enabled! ", default=some("10"))
+    option("-w", "--wordlist", help="Specify path to wordlist", required=true)
+    option("-u", "--url", help="Specify URL", required=true)
+    option("-o", "--output", help="Specify an output file", default=some("stdout"))
+    option("-x", "--extension", help="Specify extension to append to wordlist", multiple=true)
+    option("--dt", help="Specify delay between thread creation in seconds", default=some("0"))
+    option("--dr", help="Specify delay between each HTTP request in seconds", default=some("1"))
+    option("--ua", help="Specify a user agent", default=some("NimBust/1.0.0"))
+    option("--timeout", help="Specify HTTP request timeout in seconds", default=some("10"))
+  
+  var cmdLineOpt: tuple[doAppend: bool, threads: int, wordlist: string, url: string, output: string, extension: seq[string], tDelay: int, rDelay: float, timeout: int, userAgent: string, quietMode: bool]
+  
   try:
-    for line in wordlist.lines: 
-      if line.startsWith("#") or line == "": continue
-      fullList.add(line)
-  except: stdout.styledWriteLine(fgRed, fmt "Cannot open {wordlist}")
-  return fullList.distribute(thrNum)
+    var opts = p.parse()
+    cmdLineOpt = (doAppend: opts.append, 
+      threads: opts.threads.parseInt, 
+      wordlist: opts.wordlist, 
+      url: opts.url, 
+      output: opts.output, 
+      extension: opts.extension, 
+      tDelay: opts.dt.parseInt, 
+      rDelay: opts.dr.parseFloat, 
+      timeout: opts.timeout.parseInt, 
+      userAgent: opts.ua,
+      quietMode: opts.quiet)
+  except ShortCircuit as err:
+    if err.flag == "argparse_help":
+        echo p.help
+        quit(1)
+  except UsageError:
+    stderr.writeLine getCurrentExceptionMsg()
+    quit(1)
+  except ValueError:
+    stdout.writeLine "Error: ", getCurrentExceptionMsg()
+    quit(1)
+  except:
+    stdout.writeLine "Error: ", getCurrentExceptionMsg()
+    quit(1)
+    
+  var threads = newSeq[Thread[ThreadData]](cmdLineOpt.threads)
+  main(cmdLineOpt)
 
-proc bruteForce(wordlist: seq[string], baseUrl: string): void {.gcsafe.} =
+proc parseWordList(wordlist: string, thrNum: int): seq[seq[string]] =  
+  var completeList: seq[string]
+  try: 
+    completeList = wordlist.lines.toSeq.filterIt(
+       not it.startsWith("#") and not it.isEmptyOrWhitespace
+      )
+  except IOError:
+    stdout.styledWriteLine(fgRed, "(Error)", fgDefault, fmt "Cannot open {wordlist}")
+    quit(1)
+  return completeList.distribute(thrNum)
+  
+proc bruteForce(options: tuple): void =
   var 
     url: string
+    encodedWord: string
+    appendedWord: string
     response: Response
   
-  let client = newHttpClient(userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:110.0) Gecko/20100101 Firefox/110.0", sslContext=newContext(verifyMode=CVerifyPeer), timeout=10000)
-  
-  for word in wordlist:
+  let client = newHttpClient(
+    userAgent=options.userAgent, 
+    sslContext=newContext(verifyMode=CVerifyPeer), 
+    timeout=options.timeout*1000,
+    maxRedirects=0
+    )  
+    
+  proc saveToFile(value: string): void =
     try:
-      #if not word.endsWith('/'): url = fmt "{baseUrl}/{word}/"
-      #else: url = fmt "{baseUrl}/{word}"
-      
-      url = fmt "{baseUrl}/{word}"
-      response = client.request(url, HttpHead)
-      defer: client.close()
-      
-      if "200" in response.status: stdout.styledWriteLine(fgGreen, fmt "({response.status.splitWhitespace()[0]})", fgWhite, fmt "\t/{word}")
-        
-      elif "201" in response.status: stdout.styledWriteLine(fgGreen, fmt "({response.status.splitWhitespace()[0]})", fgWhite, fmt "\t/{word}")
-        
-      elif "301" in response.status: stdout.styledWriteLine(fgYellow, fmt "({response.status.splitWhitespace()[0]})", fgWhite, fmt "\t/{word}")
-        
-      elif "302" in response.status: stdout.styledWriteLine(fgYellow, fmt "({response.status.splitWhitespace()[0]})", fgWhite, fmt "\t/{word}")
-        
-      elif "307" in response.status: stdout.styledWriteLine(fgYellow, fmt "({response.status.splitWhitespace()[0]})", fgWhite, fmt "\t/{word}")
-        
-      elif "308" in response.status: stdout.styledWriteLine(fgYellow, fmt "({response.status.splitWhitespace()[0]})", fgWhite, fmt "\t/{word}")
-        
-      elif "404" notin response.status: stdout.styledWriteLine(fgRed, fmt "({response.status.splitWhitespace()[0]})", fgWhite, fmt "\t/{word}")
-        
+      acquire(L)
+      var outputFile = open(options.output, fmAppend)
+      outputFile.writeLine(value)
+      outputFile.close()
+      release(L)
+    except IOError:
+      stdout.styledWriteLine(fgRed, fmt "(Error)", fgDefault, fmt "\t(Could not save to output file {options.output})")  
     except: 
       var error = getCurrentException()
-      stdout.styledWriteLine(fgRed, fmt "/{word}: {error.msg}")
-    #if wordlist.len > 50: sleep(1000)
+      stdout.styledWriteLine(fgRed, fmt "(Error)", fgDefault, fmt "\t({error.msg})")  
+  
+  proc doRequest(urlString: string, word: string): void =
+    try:
+      response = client.head(urlString)
+      defer: client.close()
+        
+      if not options.quietMode:
+        if response.status.contains(@["200", "201"].HttpHeaderValues): 
+          stdout.styledWriteLine(fgGreen, fmt "({response.status.splitWhitespace()[0]})", fgDefault, fmt "\t/{word}")
+        elif response.status.contains(@["301", "307", "308"].HttpHeaderValues) or "302" in response.status: 
+          stdout.styledWriteLine(fgYellow, fmt "({response.status.splitWhitespace()[0]})", fgDefault, fmt "\t/{word}")
+        
+        elif "404" notin response.status: 
+          stdout.styledWriteLine(fgRed, fmt "({response.status.splitWhitespace()[0]})", fgDefault, fmt "\t/{word}")      
+    except TimeoutError:
+      stdout.styledWriteLine(fgRed, fmt "(Error)", fgDefault, fmt "\t(Connection timed out!) \t/{word}\t")
+    except: 
+      var error = getCurrentException()
+      stdout.styledWriteLine(fgRed, fmt "(Error)", fgDefault, fmt "\t({error.msg})\t/{word}")
+    if options.output != "stdout" and "404" notin response.status: saveToFile(fmt "({response.status.splitWhitespace()[0]})\t/{word}")
+      
+  if options.extension.len != 0:
+    var newList: seq[string]
+    for word in options.wordlist:
+      encodedWord = if options.doAppend: word.encodeUrl(false) & "/" else: word.encodeUrl(false)
+      newList.add(encodedWord)
+      for extension in options.extension:
+        let appendedWord = if extension.startsWith("."): encodedWord & extension else: encodedWord & "." & extension
+        newList.add(appendedWord)
     
-proc main(): void = 
+    for word in newList:
+      doRequest(fmt "{options.baseUrl}/{word}", fmt "{word}")
+      sleep(int(options.rDelay*1000))
+  else:
+    for word in options.wordlist:
+      encodedWord = if options.doAppend: word.encodeUrl(false) & "/" else: word.encodeUrl(false)
+      doRequest(fmt "{options.baseUrl}/{encodedWord}", fmt "{encodedWord}")
+      sleep(int(options.rDelay*1000))
+    
+proc main(options: tuple): void = 
   echo fmt"""
 +=======================================================================================+
-NimBust v1.0.0
+NimBust {version}
 by Eric Holloway (@RePRGM)
 +=======================================================================================+"""
-  echo "[*] Domain: ", params[0]
-  echo "[*] Wordlist: ", params[1]
-  echo "[*] Threads: ", params[2]
-  echo "[*] Hidden: 404"
-  echo "[*] Timeout: 10s"
-  stdout.styledWriteLine(fgWhite, "[*] Status Codes: [", fgGreen, "200, 201, ", fgYellow, "301, 302, 307, 308, ", fgRed, "401, 403, 405, 410", fgWhite, "]")
+  echo "[*] Domain: ", options.url
+  echo "[*] Wordlist: ", options.wordlist
+  echo "[*] Threads: ", options.threads
+  echo fmt"[*] Timeout: {options.timeout}s"
+  stdout.styledWriteLine(fgDefault, "[*] Status Codes: [", fgGreen, "200, 201, ", fgYellow, "301, 302, 307, 308, ", fgRed, "401, 403, 405, 410", fgDefault, "]")
+  echo "[*] Hide Status Code: 404"
   
-  var subWordLists = parseWordList(params[1], parseInt(params[2]))
+  if options.extension.len != 0:
+    stdout.write("[*] Extensions: ")
+    for extension in options.extension: stdout.write(extension & " ")
+    stdout.write("\n")
+  
+  if options.output != "stdout": 
+    echo "[*] Output File: ", options.output
+    discard execCmd("echo \"NimBust Results\nUrl: $1\" > $2" % [options.url, options.output])
+    
+  if options.quietMode: echo "[*] Quiet Mode: Enabled"
+  if options.tDelay != 0: echo "[*] Thread Delay: ", options.tDelay
+  
+  echo "[*] Request Delay: ", options.rDelay, "s"
+  echo "[*] User Agent: ", options.userAgent
+  
+  var subWordLists = parseWordList(options.wordlist, options.threads)
   echo fmt """
 +=======================================================================================+
 Starting NimBust: {getDateStr()} at {getClockStr()}
 +=======================================================================================+"""
-  for i in 0 ..< parseInt(params[2]): spawn bruteForce(subWordLists[i], params[0])
-  sync()
+  if subWordLists.len < 5 or subWordLists[0].len < 1000:
+    echo "(Notice)\t(Single thread mode enabled!)"
+    for i in 0 ..< subWordLists.len:
+      bruteForce((doAppend: options.doAppend, wordlist: subWordLists[i], baseUrl: options.url, extension: options.extension, output: options.output, rDelay: options.rDelay, timeout: options.timeout, userAgent: options.userAgent, quietMode: options.quietMode))
+  else:
+    for i in 0 ..< options.threads: 
+      createThread(threads[i], bruteForce, (doAppend: options.doAppend, wordlist: subWordLists[i], baseUrl: options.url, extension: options.extension, output: options.output, rDelay: options.rDelay, timeout: options.timeout, userAgent: options.userAgent, quietMode: options.quietMode))
+      sleep(options.tDelay*1000)
+  joinThreads(threads)
+
   echo fmt """
 +=======================================================================================+
 Finished NimBust: {getDateStr()} at {getClockStr()}
 +=======================================================================================+"""
-  
-when isMainModule:
-  if params.len != 3:
-    echo fmt "Usage: {nimbust} <url> <wordlist> <threads>"
-    quit(1)
-  else: main()
- 
+  deinitLock(L)
